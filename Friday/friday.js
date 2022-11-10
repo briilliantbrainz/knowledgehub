3ebf9f4a4b8c0d7b29f85ef8754695437dc9475a
Divyansh singh <divyanshpbh29@gmail.com>
2022-11-10 20:26:45 +0530

    new feature

--- Friday/friday.js
+++ Friday/friday.js
@@ -12,9 +12,33 @@ recognition.onstart = function (){
 recognition.onresult = function(event){
     let current = event.resultIndex
     let transcript = event.results[current][0].transcript;
+    transcript = transcript.toLowerCase();
+    if(transcript.includes("hello")){
+        readOut("Hello Sir, How are you?")
+    }
+    if(transcript.includes("open youtube")){
+        readOut("openning youtube sir ")
+        window.open("https://www.youtube.com/")
+    }
+
+    if(transcript.includes("open google")){
+        readOut("openning google sir..")
+        window.open("https://www.google.com/")
+    }
+
+    if(transcript.includes("search for")){
+        readOut("Here's the results")
+        let input = transcript.split("")
+        input.splice(0,10);
+        input.pop()
+        input = input.join("").split(" ").join("+")
+        console.log(input)
+
+        window.open(`https://www.google.com/search?q=${input}`)
+    }
 
     console.log(transcript)
-    readOut(transcript)
+    //readOut(transcript)
 }
 
 recognition.onend = function(event){
