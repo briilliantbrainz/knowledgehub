5a258a4dbc4fb23ba76dccdd632818828019e150
Divyansh singh <divyanshpbh29@gmail.com>
2022-11-15 14:52:40 +0530

    new features

--- Friday/friday.js
+++ Friday/friday.js
@@ -2,34 +2,69 @@
 const startBtn = document.querySelector("#start")
 const stopBtn = document.querySelector("#stop")
 const speakBtn = document.querySelector("#speak")
-const SpeechRecognition = window.SpeechRecognition  || window.webkitSpeechRecognition;
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+document.querySelector("#start_jarvis_btn").addEventListener("click", () => {
+    recognition.start()
+})
+
+let fridaycoms = []
+fridaycoms.push("what are your commands");
+fridaycoms.push("are you there friday");
+fridaycoms.push("hello friday");
+
 
 const recognition = new SpeechRecognition()
-recognition.onstart = function (){
+recognition.onstart = function () {
     console.log("VR Active");
 }
 
-recognition.onresult = function(event){
+
+/*fridaycoms.forEach((e) => {
+    document.querySelector(".commands").innerHTML += '<p> #${e} </p> <br>'
+})*/
+
+recognition.onresult = function (event) {
     let current = event.resultIndex
     let transcript = event.results[current][0].transcript;
     transcript = transcript.toLowerCase();
-    if(transcript.includes("hello")){
+    if (transcript.includes("hello")) {
         readOut("Hello Sir, How are you?")
     }
-    if(transcript.includes("open youtube")){
+
+    if (transcript.includes("what are your commands")) {
+        readOut("Sir, These are my commands")
+        document.querySelector(".commands").style.display = "block";
+    }
+
+
+
+
+    if (transcript.includes("open youtube")) {
         readOut("openning youtube sir ")
         window.open("https://www.youtube.com/")
     }
 
-    if(transcript.includes("open google")){
+    if (transcript.includes("question 1")) {
+        readOut("Question. What were the IMF and the World Bank designed for? What made them shift their attention towards developing countries? Answer The IMF and the World Bank were designed to meet the financial requirements of industrial countries. They were known as the Bretton Woods Twins. They were not equipped to deal with the challenges of poverty and lack of development in the former colonies. Fortunately, Europe and Japan rapidly rebuilt their economies and grew less dependent on the IMF and the World Bank. As a result, IMF and the World Bank began to shift their attention more towards developing countries. The major challenge before the newly independent and developing countries was to help the majority of the people to come out of the severe poverty. Even after many years of decolonisation, the former colonial powers still controlled vital resources such as minerals and land in some of their colonies. Even the US also often enjoyed the rights to exploit natural resources of the developing countries very cheaply.The formation of the group of 77 developing countries (G-77) helped these countries to demand a New International Economic Order (NIEO) that would give them real control over their natural resources, more development assistance, fairer prices for raw materials and better access for their manufactured goods in developed countries markets.")
+
+    }
+
+
+    if (transcript.includes("open brilliant brain")) {
+        readOut("openning brilliant brainz ")
+        window.open("https://briilliantbrainz.github.io/knowledgehub/")
+    }
+
+    if (transcript.includes("open google")) {
         readOut("openning google sir..")
         window.open("https://www.google.com/")
     }
 
-    if(transcript.includes("search for")){
+    if (transcript.includes("search for")) {
         readOut("Here's the results")
         let input = transcript.split("")
-        input.splice(0,10);
+        input.splice(0, 10);
         input.pop()
         input = input.join("").split(" ").join("+")
         console.log(input)
@@ -41,22 +76,22 @@ recognition.onresult = function(event){
     //readOut(transcript)
 }
 
-recognition.onend = function(event){
-   
+recognition.onend = function (event) {
+
     console.log("VR DEACTIVE");
 }
 
-recognition.continuous =true;
+recognition.continuous = true;
 
-startBtn.addEventListener("click", ()=>{
+startBtn.addEventListener("click", () => {
     recognition.start();
 })
 
-stopBtn.addEventListener("click", ()=>{
+stopBtn.addEventListener("click", () => {
     recognition.stop();
 })
 
-function readOut(message){
+function readOut(message) {
     const speech = new SpeechSynthesisUtterance()
     const allvoices = speechSynthesis.getVoices();
     speech.voice = allvoices[4]
@@ -66,10 +101,10 @@ function readOut(message){
     console.log("Speaking Out")
 }
 
-speakBtn.addEventListener("click", ()=>{
+speakBtn.addEventListener("click", () => {
     readOut("hi Dear How are you lets code something new today ")
 })
 
-window.onload = function(){
+window.onload = function () {
     readOut(" ")
 }
